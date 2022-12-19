953533a3420fee782d3b5d1c5bc69bbccfe8d570
Rojin-dumre <80736272+Rojin-dumre@users.noreply.github.com>
2022-12-19 11:46:40 +0545

    Update MongoToYaml.py

--- ProjectAutomation/MongoToYaml.py
+++ ProjectAutomation/MongoToYaml.py
@@ -3,6 +3,7 @@ from bson.json_util import dumps, loads
 import json
 import yaml
 from yaml.loader import SafeLoader
+import os
 
 client = MongoClient("localhost", 27017)
 
@@ -19,18 +20,29 @@ list_cur = list(cursor)
 #json_data = dumps(list_cur)
 #data = json.loads(json_data)
 
-yaml_data = yaml.dump(list_cur[0], explicit_start = True, explicit_end = False, sort_keys=False, indent =3, default_style=None)
+yaml_data = yaml.dump(list_cur[0], explicit_start = False, explicit_end = False, sort_keys=False, indent =3, default_style=None)
 
 
-
+ 
 with open('data.txt', 'w') as file:
     file.write(yaml_data)
 
-with open('data.txt', 'r') as f, open('dataout.yaml', 'w') as fo:
+with open('data.txt', 'r') as f, open('dataout.txt', 'w') as fo:
     for line in f:
         fo.write(line.replace("'",""))
+        #fo.write(line.replace("- ","\n- "))
 
-#db = client.movie_db
-#coll1 = db.movies
-#for document in coll1.find():
-    #print(document)
+with open('dataout.txt', 'r') as g, open('hosts.yaml', 'w') as go:
+    for line in g:
+        go.write(line.replace("-", '\n\n      -'))
+
+if os.path.exists("data.txt"):
+    os.remove("data.txt")
+else:
+    print("no file")
+
+if os.path.exists("dataout.txt"):
+    os.remove("dataout.txt")
+else:
+    print("no file")
+
