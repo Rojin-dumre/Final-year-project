cfb01e52ab16da913908e2b293b0c6d9bacc9d3d
Rojin-dumre <80736272+Rojin-dumre@users.noreply.github.com>
2022-12-21 15:09:04 +0545

    Update MongoToYaml.py

--- ProjectAutomation/MongoToYaml.py
+++ ProjectAutomation/MongoToYaml.py
@@ -2,15 +2,18 @@ from pymongo import MongoClient
 from bson.json_util import dumps, loads
 import json
 import yaml
-from yaml.loader import SafeLoader
+# from yaml.loader import SafeLoader
 import os
 import subprocess
+import re
+
+
 
 client = MongoClient("localhost", 27017)
 
 database = client["jsontoyaml"]
 
-movies = database.infodatas
+movies = database.details
 
 
 
@@ -35,19 +38,35 @@ with open('data.txt', 'r') as f, open('dataout.txt', 'w') as fo:
 
 with open('dataout.txt', 'r') as g, open('databuff.txt', 'w') as go:
     for line in g:
-        go.write(line.replace("-  ", ""))
+        go.write(line.replace("-  Router:", ""))
 
 with open('databuff.txt', 'r') as h, open('datafinal.txt', 'w') as ho:
     for line in h:
-        ho.write(line.replace("__v: 0", ""))
+        global line1
+        global line3
+        line3 = None
+        line1 = None
+        if re.search('R.+', line):
+            line3 = line
+            #print(line3)
+            line2 = str(line.strip()) + ":"
+            line1 = line2.replace("- ", "")
+            #print(line)
+            #print(line1)
+        ho.write(line.replace(str(line3), str(line1)+"\n\n"))
 
-directory = '/home/rojin'
+directory = '/home/rojin/Documents/jsontoyamlv4/jsontoyaml/python'
 filename = "hosts.yaml"
 file_path = os.path.join(directory, filename)
 
-with open('datafinal.txt', 'r') as i, open(file_path, 'w') as io:
+with open('datafinal.txt', 'r') as i, open('datafinal1.txt', 'w') as io:
     for line in i:
-        io.write(line.replace("groups: ", 'groups:\n\n         - '))
+        io.write(line.replace("   - ", "\n      - "))
+
+
+with open('datafinal1.txt', 'r') as j, open(file_path, 'w') as jo:
+    for line in j:
+        jo.write(line.replace("   __v: 0", ''))
 
 #filename = 'hosts.txt'
 #line = subprocess.check_output(['head', '-n', '-1', filename])
@@ -57,26 +76,30 @@ with open('datafinal.txt', 'r') as i, open(file_path, 'w') as io:
     #file.write(line)
 
 
-if os.path.exists("dataout.txt"):
-    os.remove("dataout.txt")
+if os.path.exists("data.txt"):
+   os.remove("data.txt")
 else:
     print("no file")
 
 if os.path.exists("databuff.txt"):
     os.remove("databuff.txt")
 else:
-    print("no file")
+   print("no file")
 
 if os.path.exists("datafinal.txt"):
-    os.remove("datafinal.txt")
+   os.remove("datafinal.txt")
+else:
+   print("no file")
+
+if os.path.exists("dataout.txt"):
+   os.remove("dataout.txt")
+else:
+   print("no file")
+
+if os.path.exists("datafinal1.txt"):
+   os.remove("datafinal1.txt")
 else:
     print("no file")
-
-if os.path.exists("data.txt"):
-    os.remove("data.txt")
-else:
-    print("no file")
-
 #db = client.movie_db
 #coll1 = db.movies
 #for document in coll1.find():
